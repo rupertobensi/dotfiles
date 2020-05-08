d68cb71ddb51ee32733a3b5e81054425870b4ced
rupertobensi <lajenz@gmail.com>
2020-05-09 00:20:22 +0200

    changes in dmenu scripts, new polybar

--- .config/polybar/polybar-scripts/openweathermap-detailed.sh
+++ .config/polybar/polybar-scripts/openweathermap-detailed.sh
@@ -2,23 +2,23 @@
 
 get_icon() {
     case $1 in
-        01d) icon="";;
-        01n) icon="";;
-        02d) icon="";;
-        02n) icon="";;
-        03*) icon="";;
-        04*) icon="";;
-        09d) icon="";;
-        09n) icon="";;
-        10d) icon="";;
-        10n) icon="";;
-        11d) icon="";;
-        11n) icon="";;
-        13d) icon="";;
-        13n) icon="";;
-        50d) icon="";;
-        50n) icon="";;
-        *) icon="";
+        01d) icon="盛";;
+        01n) icon="";;
+        02d) icon="";;
+        02n) icon="";;
+        03*) icon="摒";;
+        04*) icon="";;
+        09d) icon="";;
+        09n) icon="";;
+        10d) icon="";;
+        10n) icon="";;
+        11d) icon="";;
+        11n) icon="";;
+        13d) icon="";;
+        13n) icon="";;
+        50d) icon="";;
+        50n) icon="";;
+        *) icon="盛";
     esac
 
     echo $icon
@@ -55,5 +55,5 @@ if [ -n "$weather" ]; then
     weather_temp=$(echo "$weather" | jq ".main.temp" | cut -d "." -f 1)
     weather_icon=$(echo "$weather" | jq -r ".weather[0].icon")
 
-    echo "$(get_icon "$weather_icon")" "$weather_desc", "$weather_temp$SYMBOL"
+    echo "$(get_icon "$weather_icon") " "$weather_desc", "$weather_temp$SYMBOL"
 fi
